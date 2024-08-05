ddedd070380a5cc33b17f095a1e2e3883a8a8de0
Xeno333 <Xeno333@mailfence.com>
2024-08-05 07:39:41 -0500

    make node def file

--- src/mapgen/biome_plains.lua
+++ src/mapgen/biome_plains.lua
@@ -1,11 +1,12 @@
-
-
+--[[
 	minetest.register_decoration({
 		name = "mcl_better_end:end_grass",
 		deco_type = "simple",
 		place_on = {"mcl_end:end_stone"},
 		flags = "all_floors",
 		sidelen = 16,
+		y_min = mcl_vars.mg_end_min,
+		y_max = mcl_vars.mg_end_max,
 		noise_params = {
 			offset = -0.012,
 			scale = 0.024,
@@ -14,10 +15,9 @@
 			octaves = 3,
 			persist = 0.6
 		},
-		y_min = mcl_vars.mg_end_min,
-		y_max = mcl_vars.mg_end_max,w
 		decoration = "mcl_flowers:tallgrass",
 		height = 1,
-		height_max = 8,
-		biomes = {"End", "EndMidlands", "EndHighlands", "EndBarrens", "EndSmallIslands"},
+		height_max = 1,
+		biomes = {"EndSmallIslands"},
 	})
+]]--
