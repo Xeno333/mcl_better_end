6c7390269e0b0f2577f5b4721e33dde1236734db
Xeno333 <Xeno333@mailfence.com>
2024-08-07 22:06:34 -0500

    textures

--- init.lua
+++ init.lua
@@ -21,3 +21,4 @@ dofile(mcl_better_end.code_loc .. "/mapgen/biome_oasis.lua")
 dofile(mcl_better_end.code_loc .. "/mapgen/biome_ender_sea.lua")
 
 dofile(mcl_better_end.code_loc .. "/enderite/ore.lua")
+dofile(mcl_better_end.code_loc .. "/enderite/enderite.lua")
---
+++ src/enderite/enderite.lua
@@ -0,0 +1,7 @@
+
+
+
+minetest.register_craftitem("mcl_better_end:raw_enderite", {
+    description = "Raw Enderite",
+    inventory_image = "raw_enderite.png",
+})
