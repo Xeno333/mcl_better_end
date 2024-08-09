90807f86138e041b048be870a802bd2adc04830b
Bapt-tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-08-09 16:44:33 +0200

    Create nephrite_craft.lua

---
+++ src/nephrite/nephrite_craft.lua
@@ -0,0 +1,36 @@
+-- Recettes pour les outils
+minetest.register_craft({
+    output = 'mcl_better_end:nephrite_pickaxe',
+    recipe = {
+        {'mcl_better_end:nephrite_ingot', 'mcl_better_end:nephrite_ingot', 'mcl_better_end:nephrite_ingot'},
+        {'', 'mcl_core:stick', ''},
+        {'', 'mcl_core:stick', ''},
+    }
+})
+
+minetest.register_craft({
+    output = 'mcl_better_end:nephrite_axe',
+    recipe = {
+        {'', 'mcl_better_end:nephrite_ingot', 'mcl_better_end:nephrite_ingot'},
+        {'', 'mcl_core:stick', 'mcl_better_end:nephrite_ingot'},
+        {'', 'mcl_core:stick', ''},
+    }
+})
+
+minetest.register_craft({
+    output = 'mcl_better_end:nephrite_sword',
+    recipe = {
+        {'', 'mcl_better_end:nephrite_ingot', ''},
+        {'', 'mcl_better_end:nephrite_ingot', ''},
+        {'', 'mcl_core:stick', ''},
+    }
+})
+
+minetest.register_craft({
+    output = 'mcl_better_end:nephrite_shovel',
+    recipe = {
+        {'', 'mcl_better_end:nephrite_ingot', ''},
+        {'', 'mcl_core:stick', ''},
+        {'', 'mcl_core:stick', ''},
+    }
+})
