ca9ebdb81788fe8ee42bf7fc7ebb59de040cc6e9
Xeno333 <Xeno333@mailfence.com>
2024-08-12 15:57:04 -0500

    update

--- src/enderite/ore.lua
+++ src/enderite/ore.lua
@@ -9,7 +9,8 @@ mcl_better_end.mapgen.ores.enderite = function(data, vi, area, pr, x, y, z)
     end
     --enderite
     if pr:next(1, 1000) == 5 then
-        if pr:next(1, 10) == 5 then
+        if pr:next(1, 50) == 5 then
+            print("BBB")
             data[vi] = mcl_better_end.mapgen.registered_nodes.enderite_ore
         end
     end
--- src/nephrite/ore.lua
+++ src/nephrite/ore.lua
@@ -1,15 +1,16 @@
 local YMAX = -26920
-local YMIN = -27900
+local YMIN = -26900
 
 mcl_better_end.mapgen.registered_nodes.nephrite_ore = minetest.get_content_id("mcl_better_end:nephrite_ore")
 
-mcl_better_end.mapgen.ores.enderite = function(data, vi, area, pr, x, y, z)
+mcl_better_end.mapgen.ores.nephrite = function(data, vi, area, pr, x, y, z)
     if y > YMAX or y < YMIN then
         return
     end
-    --enderite
+    --nephrite
     if pr:next(1, 1000) == 5 then
-        if pr:next(1, 10) == 5 then
+        if pr:next(1, 100) == 5 then
+            print("AAA")
             data[vi] = mcl_better_end.mapgen.registered_nodes.nephrite_ore
         end
     end
