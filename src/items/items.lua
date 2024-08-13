2a48941618eeb0dffe866b9c7eee8f1d1ee6fd34
Xeno333 <Xeno333@mailfence.com>
2024-08-13 17:25:38 -0500

    update

--- src/items/items.lua
+++ src/items/items.lua
@@ -9,6 +9,6 @@ minetest.register_on_mods_loaded(function()
     local endermite_def = minetest.registered_entities["mobs_mc:endermite"]
 
     if endermite_def then
-        table.insert(endermite_def.drops, {name = "mcl_better_end:ender_shard", chance = 100})
+        table.insert(endermite_def.drops, {name = "mcl_better_end:ender_shard", chance = 600})
     end
 end)
