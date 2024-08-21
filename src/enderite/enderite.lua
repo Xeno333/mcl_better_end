3a5853b9aa21b896c976ff20b8d77df8f4642f74
Xeno333 <149852758+Xeno333@users.noreply.github.com>
2024-08-21 14:08:15 -0500

    Update enderite.lua

--- src/enderite/enderite.lua
+++ src/enderite/enderite.lua
@@ -102,28 +102,42 @@ minetest.register_tool("mcl_better_end:enderite_sword", {
     description = "Enderite Sword",
     inventory_image = "enderite_sword.png",
     _mcl_toollike_wield = true,
-    tool_capabilities = {
-        full_punch_interval = 1.0,
-        max_drop_level = 1,
-        groupcaps = {
-            choppy = {times={[1]=0.1, [2]=0.1, [3]=0.1}, uses=3000, maxlevel=30},
-        },
-        damage_groups = {fleshy=10},
-    },
+	wield_scale = cl_vars.tool_wield_scale,
+	groups = { weapon=1, sword=1, dig_speed_class=5, enchantability=10 },
+	tool_capabilities = {
+		full_punch_interval = 0.600,
+		max_drop_level=5,
+		damage_groups = {fleshy=10},
+		punch_attack_uses = 2100,
+	},
+	sound = { breaks = "default_tool_breaks" },
+	on_place = mcl_tools.tool_place_funcs.sword,
+	_repair_material = "mcl_better_end:enderite_ingot",
+	_mcl_toollike_wield = true,
+	_mcl_diggroups = {
+		swordy = { speed = 8, level = 5, uses = 2100 },
+		swordy_cobweb = { speed = 8, level = 5, uses = 2100 }
+	},
 })
 
 minetest.register_tool("mcl_better_end:enderite_shovel", {
     description = "Enderite Shovel",
     inventory_image = "enderite_shovel.png",
     _mcl_toollike_wield = true,
-    tool_capabilities = {
-        full_punch_interval = 1.0,
-        max_drop_level = 1,
-        groupcaps = {
-            choppy = {times={[1]=0.1, [2]=0.1, [3]=0.1}, uses=300, maxlevel=30},
-        },
-        damage_groups = {fleshy=7},
-    },
+	groups = { tool=1, shovel=1, dig_speed_class=6, enchantability=10},
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=5,
+		damage_groups = {fleshy=5},
+		punch_attack_uses = 2100,
+	},
+	on_place = mcl_tools.tool_place_funcs.shovel,
+	sound = { breaks = "default_tool_breaks" },
+	_repair_material = "mcl_better_end:enderite_ingot",
+	_mcl_toollike_wield = true,
+	_mcl_diggroups = {
+		shovely = { speed = 10, level = 6, uses = 2100 }
+	},
 })
 
 
