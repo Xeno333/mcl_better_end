014bd4d283da14aef617293c9a5d0ae524803015
Xeno333 <Xeno333@mailfence.com>
2024-08-27 23:12:45 -0500

    update

--- src/mapgen/mapgen.lua
+++ src/mapgen/mapgen.lua
@@ -98,7 +98,7 @@ function mcl_better_end.mapgen.gen(minp, maxp, seed)
             for z = maxp.z, minp.z, -1 do
                 for x = maxp.x, minp.x, -1 do
                     local vi = area:index(x, y, z)
-                    local noise = perlin_l:get_3d({x = x, y = y, z = z})
+                    local noise = 0.4
                     
                     if not mcl_better_end.api.is_island(noise) then
                         data[vi] = mcl_better_end.mapgen.registered_nodes.air
@@ -146,11 +146,11 @@ function mcl_better_end.mapgen.gen(minp, maxp, seed)
                     noises.l[x][y + 1] = {}
                 end
                 if not noises.l[x][y + 1][z] then
-                    noises.l[x][y + 1][z] = perlin_l:get_3d({x = x, y = y + 1, z = z})
+                    noises.l[x][y + 1][z] = 0.4
                 end
 
-                local noise_center = perlin:get_3d({x = x, y = y, z = z})
-                local noise = perlin_l:get_3d({x = x, y = y, z = z})
+                local noise_center = 0.4
+                local noise = 0.4
                 local noise2 = noises.l[x][y + 1][z]
 
                 noises.l[x][y][z] = noise
